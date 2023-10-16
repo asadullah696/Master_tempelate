08418d654273476bb709a0f029b41777871511e5
asadullah696 <asadullahrajpoot19@gmail.com>
2023-10-17 01:52:11 +0500

    Some Progress

--- JavaScript/linking.js
+++ JavaScript/linking.js
@@ -2,11 +2,49 @@
 
 
 let menuListHome = () =>{
-    document.getElementById("homePage").style.display = "flex"
-    document.getElementById("profilePage").style.display = "none"
+    document.getElementById("homePage").style.display = "flex";
+    document.getElementById("marketPage").style.display = "none";
+    document.getElementById("walletPage").style.display = "none";
+    document.getElementById("profilePage").style.display = "none";
+    document.getElementById("navMenuHome").style.backgroundColor = "rgba(6, 127, 132, 0.623)";
+    document.getElementById("navMenuMarket").style.backgroundColor = "aqua";
+    document.getElementById("navMenuWallet").style.backgroundColor = "aqua";
+    document.getElementById("navMenuHelpCenter").style.backgroundColor = "aqua";
+    document.getElementById("navMenuProfile").style.backgroundColor = "aqua";
+}
+
+let menuListMarket = () =>{
+    document.getElementById("homePage").style.display = "none";
+    document.getElementById("marketPage").style.display = "flex";
+    document.getElementById("walletPage").style.display = "none";
+    document.getElementById("profilePage").style.display = "none";
+    document.getElementById("navMenuHome").style.backgroundColor = "aqua";
+    document.getElementById("navMenuMarket").style.backgroundColor = "rgba(6, 127, 132, 0.623)";
+    document.getElementById("navMenuWallet").style.backgroundColor = "aqua";
+    document.getElementById("navMenuHelpCenter").style.backgroundColor = "aqua";
+    document.getElementById("navMenuProfile").style.backgroundColor = "aqua";
+}
+
+let menuListWallet = () =>{
+    document.getElementById("homePage").style.display = "none";
+    document.getElementById("marketPage").style.display = "none";
+    document.getElementById("walletPage").style.display = "flex";
+    document.getElementById("profilePage").style.display = "none";
+    document.getElementById("navMenuHome").style.backgroundColor = "aqua";
+    document.getElementById("navMenuMarket").style.backgroundColor = "aqua";
+    document.getElementById("navMenuWallet").style.backgroundColor = "rgba(6, 127, 132, 0.623)";
+    document.getElementById("navMenuHelpCenter").style.backgroundColor = "aqua";
+    document.getElementById("navMenuProfile").style.backgroundColor = "aqua";
 }
 
 let menuListPortfolio = () =>{
-    document.getElementById("homePage").style.display = "none"
-    document.getElementById("profilePage").style.display = "flex"
+    document.getElementById("homePage").style.display = "none";
+    document.getElementById("marketPage").style.display = "none";
+    document.getElementById("walletPage").style.display = "none";
+    document.getElementById("profilePage").style.display = "flex";
+    document.getElementById("navMenuHome").style.backgroundColor = "aqua";
+    document.getElementById("navMenuMarket").style.backgroundColor = "aqua";
+    document.getElementById("navMenuWallet").style.backgroundColor = "aqua";
+    document.getElementById("navMenuHelpCenter").style.backgroundColor = "aqua";
+    document.getElementById("navMenuProfile").style.backgroundColor = "rgba(6, 127, 132, 0.623)";
 }
