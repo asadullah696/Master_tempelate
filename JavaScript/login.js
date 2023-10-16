140c6bd182533704f2badde6cdf0b4beb979d15f
asadullah696 <asadullahrajpoot19@gmail.com>
2023-10-17 02:18:24 +0500

    User Verification

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -23,3 +23,34 @@ console.log(userProfile)
 let savingData = () =>{
     userProfile.push(userData)
 }
+
+
+        let email = ["asadullah", "badshah", "Kings"]
+        let password = ["123", "456", "789"]
+
+        // Login Function
+
+        const checkValidation = () =>{
+        let email = ["asadullah", "badshah", "Kings"]
+        let password = ["123", "456", "789"]
+        
+        let useremail = document.getElementById("emailLogin").value ;
+        let userpassword = document.getElementById("passwordLogin").value ;
+        
+        let emailCheck = !email.includes(useremail);
+        console.log(emailCheck)
+        let passwordCheck = !password.includes(userpassword);
+
+        if(email.includes(useremail) && password.includes(userpassword)){
+            true
+            document.getElementById("loginPass").href = "./Html/main.html"
+        }
+        // else{
+        //     if(emailCheck == true){
+        //         document.getElementById("erroremail").style.display = "flex"
+        //     }
+
+        //     if(passwordCheck == true){
+        //         document.getElementById("errorpassword").style.display = "flex"
+        //     }
+        }
